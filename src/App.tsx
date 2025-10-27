46d96dce3d1ab1d394b49ecdeb0ecfa7b87b00e1
Abhishek <abhi761685@gmail.com>
2025-10-27 15:45:44 +0530

    add auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import Dashboard from "./pages/dashboard";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 
 export default function App () {
   return (
-    <Dashboard />
+   
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      </Routes> 
+    </BrowserRouter>
+    
   )
 }
