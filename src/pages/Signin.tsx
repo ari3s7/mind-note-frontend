4796c1039c427f50c2116a69decc0318ede2f285
Abhishek <abhi761685@gmail.com>
2025-10-28 14:06:17 +0530

    add auth pages

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,76 +1,20 @@
+import { Button } from "../components/Button";
+import { InputBox } from "../components/InputBox";
 
-
-
-
-
-
-
-
-export function Signin() {
-  return (
-    
-    <div className="bg-blackc ">
-      <div className="flex max-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-black">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+export function Signin () {
+    return <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
+        <div className="bg-white rounded border min-w-100 min-h-90">
+            <div className="flex pt-20">
+                <InputBox placeholder="Username" />
+            </div>
+           <div className="pt-10">
+            <InputBox placeholder="Password" />
+           </div>
+           
+            <div className="flex justify-center pt-10">
+                <Button variant="primary" text="Signin" />
+            </div>
+           
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
-            <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          
-          
-        </div>
-      </div>
-      </div>
-    
-  )
+    </div>
 }
